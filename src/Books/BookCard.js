baff55bb8613b3e46cce754acc28d459fe929fbb
Amber Rudy <59670802+amberrudy@users.noreply.github.com>
2020-02-19 18:18:40 -0500

    bookscreated

--- src/Books/BookCard.js
+++ src/Books/BookCard.js
@@ -3,7 +3,7 @@ import { Card,Button } from "react-bootstrap";
 
 export function BookCard(props) {
     return (
-        <Card style {{ width: "16em"}}>
+        <Card style={{ width: "16em"}}>
             <Card.Img variant="top" src="http://via.placeholder.com/150" />
             <Card.Body>
                 <Card.Title>{props.book.title}</Card.Title>
--- src/Books/BookDisplay.js
+++ src/Books/BookDisplay.js
@@ -3,11 +3,11 @@ import { BookCard } from "./BookCard";
 import { CardColumns } from "react-bootstrap";
 
 export function BookDisplay(props) {
-    return (
+ return (
         <div className="display">
             <h3>Books</h3>
             <CardColumns>
-                {props.books.map(b ==> (
+                {props.books.map(b => (
                     <BookCard book={b} key={b.id} />
                 ))}
             </CardColumns>
