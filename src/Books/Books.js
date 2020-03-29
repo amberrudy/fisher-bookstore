8dc0e2ae71d8bdd4efaadfa3ed6266135d6755c9
amberrudy <59670802+amberrudy@users.noreply.github.com>
2020-03-29 15:10:47 -0400

    Update Books.js

--- src/Books/Books.js
+++ src/Books/Books.js
@@ -1,59 +1,20 @@
-import React, { Component } from "react";
-import "./Books.css";
-import { BookDisplay } from "./BookDisplay";
+import React, { useEffect, useState } from "react";
+import { BookDisplay } from "./BookDisplay"
 
-export default class Books extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            books: [
-                {
-                    id: 1,
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id: 3,
-                    title: "Amber's Life Story",
-                    author: "Amber Rudy",
-                    isbn: "978-0337432198"
-                },
-                {
-                    id: 4,
-                    title: "The Tale of Amber",
-                    author: "Amber Roodie",
-                    isbn: "978-7382934501"
-                },
-                {
-                    id: 5,
-                    title: "Amber and 3610",
-                    author: "Rebma Ydur",
-                    isbn: "978-0324567123"
-                },
-                {
-                    id: 6,
-                    title: "Amber Might Fail This Class",
-                    author: "Amber Ydur",
-                    isbn: "978-1967301239"
-                }
-            ]
-        };
-    }
-    render() {
-            return (
-                <div className="Books">
-                    <div className="lander">
-                     <BookDisplay books={this.state.books} />
-                    </div>
-                </div>
-            );
-     }  
+export default function Books(props) {
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+        fetch("https://localhost:5001/api/books/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
+    return (
+        <div className="Books">
+            <div className="lander">
+                <BookDisplay books={data} />
+            </div>
+        </div>
+    );
 }
