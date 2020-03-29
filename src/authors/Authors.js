01d92589ae23109962431b26eded00c3a0c7a797
amberrudy <59670802+amberrudy@users.noreply.github.com>
2020-03-29 15:11:45 -0400

    Update Authors.js

--- src/authors/Authors.js
+++ src/authors/Authors.js
@@ -1,59 +1,20 @@
-import React, { Component } from "react";
-import "./Authors.css";
-import { AuthorDisplay } from "./AuthorDisplay";
+import React, { useEffect, useState } from "react";
+import { AuthorDisplay } from "./AuthorDisplay"
 
-export default class Authors extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authors: [
-                {
-                    id: 1,
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id: 3,
-                    title: "Amber's Life Story",
-                    author: "Amber Rudy",
-                    isbn: "978-0337432198"
-                },
-                {
-                    id: 4,
-                    title: "The Tale of Amber",
-                    author: "Amber Roodie",
-                    isbn: "978-7382934501"
-                },
-                {
-                    id: 5,
-                    title: "Amber and 3610",
-                    author: "Rebma Ydur",
-                    isbn: "978-0324567123"
-                },
-                {
-                    id: 6,
-                    title: "Amber Might Fail This Class",
-                    author: "Amber Ydur",
-                    isbn: "978-1967301239"
-                }
-            ]
-        };
-    }
-    render() {
-            return (
-                <div className="Authors">
-                    <div className="lander">
-                     <AuthorDisplay authors={this.state.authors} />
-                    </div>
-                </div>
-            );
-     }  
+export default function Authors(props) {
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+        fetch("https://localhost:5001/api/authors/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+
+    return (
+        <div className="Authors">
+            <div className="lander">
+                <AuthorDisplay authors={data} />
+            </div>
+        </div>
+    );
 }
